e964c46a4b7fc52ad0ebd08e0b35047978639c57
jamiegyoung22 <jamiegyoung22@gmail.com>
2023-01-27 02:28:25 +0000

    fix(merge): fix merge issues

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -1,10 +1,12 @@
 import Title from '@/components/atoms/Title';
 import styles from '@/components/molecules/NavBar.module.css';
+import Link from 'next/link';
 
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
       <Title />
+      <Link href={`/tags`}>Bank Layouts</Link>
     </div>
   );
 }
